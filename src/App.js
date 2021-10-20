0ae83af0c06bc95894f751fe4baa0637af6b1a79
Shabbir16 <pshabbir36@gmail.com>
2021-10-20 17:38:44 +0530

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import "./style.css";
+import ExpenseItems from './components/ExpenseItems';
 
 export default function App() {
   return (
     <div>
       <h1>Hello StacsdfkBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <ExpenseItems/>
     </div>
   );
 }
---
+++ src/components/ExpenseItems.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function ExpenseItems(){
+  return <h1>Expense</h1>;
+}
+
+export default ExpenseItems;
