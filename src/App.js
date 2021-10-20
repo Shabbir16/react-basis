6310b28d2cfa0260fec95c715059b731f9b8da83
Shabbir16 <pshabbir36@gmail.com>
2021-10-20 17:42:11 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import './style.css';
 import ExpenseItems from './components/ExpenseItems';
 
 export default function App() {
+  let expenseArr = [
+    {
+      id: 1,
+      title: 'Cars Insaurance',
+      date: new Date(2021, 11, 21),
+      amount: 229.99,
+    },
+  ];
+
+  let arr = [];
+  for (let i = 0; i < expenseArr.length; i++) {
+    arr.push(
+      <ExpenseItems
+        title={expenseArr[i].title}
+        date={expenseArr[i].date}
+        amount={expenseArr[i].amount}
+      />
+    );
+  }
+
   return (
     <div>
       <h1>Hello StacsdfkBlitz!</h1>
-      <ExpenseItems/>
+      {arr}
     </div>
   );
 }
